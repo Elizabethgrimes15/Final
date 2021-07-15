f9393b7dd1ebe070f9a26512df02cbbcb4e55a59
Elizabethgrimes15 <80325604+Elizabethgrimes15@users.noreply.github.com>
2021-07-15 23:05:11 +0300

    Add files via upload

---
+++ examHomework/features/pageObjects/seleniumEasy.page.ts
@@ -0,0 +1,292 @@
+import { seleniumEasyRepository } from "../objectRepository/seleniumEasy.obj";
+import { BrowserHacks } from "../../support/browserHacks";
+import { browser, by, ExpectedConditions } from "protractor";
+import { expect } from "chai";
+
+
+const defaultTimeout = browser.params.defaultTimeout;
+
+export class SeleniumEasyPage {
+
+    seleniumEasyRepo: seleniumEasyRepository;
+
+    constructor () {
+        this.seleniumEasyRepo = new seleniumEasyRepository;
+    }
+
+    public async clearBrowserData() {
+        let browserHacks = new BrowserHacks;
+        browserHacks.ClearBrowserData();
+    }
+
+    //Checkbox
+    public async goTocheckboxSeleniumEasy() {
+        await browser.navigate().to(browser.params.checkboxSeleniumEasy);
+    }
+    public async checkboxPick(){
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.checkboxdemo), defaultTimeout, "Time out");
+        await this.seleniumEasyRepo.checkboxdemo.click();
+    }
+    public async checkboxChoice(){
+        expect( await this.seleniumEasyRepo.checkboxdemo.isSelected()).equal(true);
+    }
+
+    //Radio Button 
+
+    public async goToradiobuttonSeleniumEasy() {
+        await browser.navigate().to(browser.params.radiobuttonSeleniumEasy);
+    }
+
+    public async radioPick1() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.radiobuttonsex), defaultTimeout, "Time out");
+        await this.seleniumEasyRepo.radiobuttonsex.click();
+    
+    }
+    public async radioPick2() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.radiobuttonage), defaultTimeout, "Time out");
+        await this.seleniumEasyRepo.radiobuttonage.click();
+    
+    }
+        public async buttonConfirm() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.confirmbutton), defaultTimeout, "Time out");
+        await this.seleniumEasyRepo.confirmbutton.click();
+    }
+    
+    public async radioSelect(){
+        expect( await this.seleniumEasyRepo.radiobuttonsex.isSelected()).equal(true);
+        expect( await this.seleniumEasyRepo.radiobuttonage.isSelected()).equal(true);
+    }
+    //DDL
+
+    public async goToddlSeleniumEasy() {
+        await browser.navigate().to(browser.params.ddlSeleniumEasy);
+    }
+    public async ddlExpand() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.ddldemo), defaultTimeout, "Time out");
+        await this.seleniumEasyRepo.ddldemo.click();
+    }
+    public async ddlPick() {
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.sundaypick), defaultTimeout, "Time out");    
+    await this.seleniumEasyRepo.sundaypick.click();
+    }
+
+    public async ddlPickconfirm(){
+        expect( await this.seleniumEasyRepo.sundaypick.isSelected()).equal(true);
+    }
+
+    //Input
+    public async goToinputSeleniumEasy() {
+    await browser.navigate().to(browser.params.inputSeleniumEasy);
+    }
+    public async typeIntoInput() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.inputdemo), defaultTimeout, "Time out");
+        await browser.actions().mouseMove(this.seleniumEasyRepo.inputdemo).click().sendKeys('Test').perform()
+    }
+
+    public async buttonInputClick() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.inputbuttonclick), defaultTimeout, "Time out");
+        await browser.actions().mouseMove(this.seleniumEasyRepo.inputbuttonclick).click().perform();
+
+    }
+    public async buttonInputShow(){
+        await browser.wait(ExpectedConditions.presenceOf(await this.seleniumEasyRepo.inputdisplay), defaultTimeout, "Текст не введен");
+    }
+    //Submit Form
+    public async goTosubmitFormSeleniumEasy() {
+        await browser.navigate().to(browser.params.submitFormSeleniumEasy);
+        }
+       
+        public async nameFieldTyping() {
+            await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.nameinput), defaultTimeout, "Time out");
+            await browser.actions().mouseMove(this.seleniumEasyRepo.nameinput).click().sendKeys('Liza').perform();
+        }
+        public async submitButtonPress() {
+            await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.submitbuttonelem), defaultTimeout, "Time out");
+            await browser.actions().mouseMove(this.seleniumEasyRepo.submitbuttonelem).click().perform();
+            }
+
+        public async loadingIconSpin(){
+            await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.spinnericon),defaultTimeout,"Имя не введено");
+        }
+//List Box Easy Demo
+public async goTolistboxeSeleniumEasy() {
+    await browser.navigate().to(browser.params.listboxeSeleniumEasy);
+    }
+   
+    public async buttonSelectAllElement() {
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.selectalldefault), defaultTimeout, "Time out");
+        await browser.actions().mouseMove(this.seleniumEasyRepo.selectalldefault).click().perform();
+    }
+
+    public async selectedBoxesAnimation(){
+        
+        await browser.wait(ExpectedConditions.presenceOf(await this.seleniumEasyRepo.selectallpicked), defaultTimeout);
+
+       
+    }
+    //File Download
+
+    public async goTofiledownloadSeleniumEasy() {
+        await browser.navigate().to(browser.params.filedownloadSeleniumEasy);
+        }
+       
+        public async exampleInputForDownload() {
+            await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.exampleinput), defaultTimeout, "Time out");
+            await browser.actions().mouseMove(this.seleniumEasyRepo.exampleinput).click().sendKeys('example').perform();
+        }
+        public async generateButtonPress() {
+            await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.generatebutton), defaultTimeout, "Time out");
+            await browser.actions().mouseMove(this.seleniumEasyRepo.generatebutton).click().perform();
+            }
+        public async downloadButtonNew() {
+            await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.downloadbutton), defaultTimeout);
+            await browser.actions().mouseMove(this.seleniumEasyRepo.downloadbutton).click().perform();
+            
+        }
+
+    //Numeric Calculator 
+    public async goToNumericInputSeleniumEasy() {
+        await browser.navigate().to(browser.params.inputSeleniumEasy);
+        }
+       
+    public async firstNumericInput() {
+        await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.inputfieldfirst), defaultTimeout);
+        await browser.actions().mouseMove(this.seleniumEasyRepo.inputfieldfirst).click().sendKeys('3').perform();
+        }
+    public async secondNumericInput() {
+        await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.inputfieldsecond), defaultTimeout);
+            await browser.actions().mouseMove(this.seleniumEasyRepo.inputfieldsecond).click().sendKeys('6').perform();
+            }
+            public async getTotalButtonPress() {
+                await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.gettotalbutton), defaultTimeout);
+                await browser.actions().mouseMove(this.seleniumEasyRepo.gettotalbutton).click().perform();
+                }
+    public async totalSumDisplay() {
+    await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.totalsumvalue));
+        }
+
+
+    //Multiple Checkboxes
+    public async goToMultipleCheckboxPage() {
+        await browser.navigate().to(browser.params.checkboxSeleniumEasy);
+    }
+    public async selectallCheckButton(){
+        await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.selectallchecks), defaultTimeout);
+        await this.seleniumEasyRepo.selectallchecks.click();
+    }
+    public async multipleCheckboxSelect(){
+        expect( await this.seleniumEasyRepo.selectallchecks.isSelected()).equal(false);
+    }
+
+//Filter Check
+    public async goToFilterList() {
+        await browser.navigate().to(browser.params.filterlist);
+    }
+    public async filterTypeCheck(){
+        await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.inputforfiltercheck), defaultTimeout, "Time out");
+        await browser.actions().mouseMove(this.seleniumEasyRepo.inputforfiltercheck).click().sendKeys('Tree').perform();
+    }
+    public async matchingFilterResults(){
+        await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.filtermatchtile), defaultTimeout);
+    }
+
+//Loading Bar
+public async goToloadingbarSeleniumEasy() {
+    await browser.navigate().to(browser.params.loadingbarSeleniumEasy);
+}
+public async startDownloadClick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.startdownloadbutton), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.startdownloadbutton).click().perform();
+}
+public async progressBarDisplay(){
+    await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.progressbarelem), defaultTimeout);
+}
+
+//Modal Window
+public async goTomodalwindowSeleniumEasy() {
+    await browser.navigate().to(browser.params.modalwindowSeleniumEasy);
+}
+public async launchModalClick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.launchmodalbutton), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.launchmodalbutton).click().perform();
+}
+public async modalWindowDisplay(){
+    await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.modalwindowelem), defaultTimeout);
+}
+public async saveChangesClick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.savechangesbutton), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.savechangesbutton).click().perform();
+}
+
+
+//Alert Message
+public async goToalertmessageSeleniumEasy() {
+    await browser.navigate().to(browser.params.alertmessageSeleniumEasy);
+}
+public async dangerMessageClick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.dangermessagebutton), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.dangermessagebutton).click().perform();
+}
+public async dangerMessageDisplay(){
+    await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.dangermessage), defaultTimeout);
+}
+public async closeButtonClick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.messageclosebutton), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.messageclosebutton).click().perform();
+}
+
+
+//Search & Select
+public async goTosearchselectSeleniumEasy() {
+    await browser.navigate().to(browser.params.searchselectSeleniumEasy);
+}
+public async ddlInputTyping(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.ddlinputfield), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.ddlinputfield).click().sendKeys('In').perform();
+}
+public async ddlValuePick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.matchddlvalue), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.matchddlvalue).click().perform();
+}
+public async ddlchoicedisplay(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.ddlpickresult), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.ddlpickresult).click().perform();
+}
+
+//Date Picker
+public async goTodatepickerSeleniumEasy() {
+    await browser.navigate().to(browser.params.datepickerSeleniumEasy);
+}
+public async selectDatePick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.datepick), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.datepick).click().perform();
+}
+public async todayButtonClick(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.todaybutton), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.todaybutton).click().perform();
+}
+public async todayDateDisplay(){
+    await browser.wait(ExpectedConditions.visibilityOf(this.seleniumEasyRepo.todaydisplay), defaultTimeout, "Time out");
+    await browser.actions().mouseMove(this.seleniumEasyRepo.todaydisplay).click().perform();
+
+}
+
+
+//public async goTologinvalidationherokuapp() {
+  //  await browser.navigate().to(browser.params.loginvalidationherokuapp);
+//}
+//public async validNameInput(){
+    
+  //  await browser.actions().click(this.seleniumEasyRepo.loginnameinput).sendKeys('Elizaveta').perform();
+//}
+//public async invalidPasswordInput(){
+   
+  //  await browser.actions().click(this.seleniumEasyRepo.passwordinput).sendKeys('1334').perform();
+//}
+//public async checkValidationMessageDisplay(){
+  //   await browser.wait(ExpectedConditions.visibilityOf(await this.seleniumEasyRepo.invalidinputmessage), defaultTimeout, "Timeout error");
+ 
+
+//}
+
+}
---
+++ examHomework/features/stepsDefinitions/gherkin.steps.ts
@@ -0,0 +1,302 @@
+import { browser, by, element, ElementFinder, ExpectedConditions, Key } from "protractor";
+import { BrowserHacks } from "../../support/browserHacks";
+import { SeleniumEasyPage } from "../pageObjects/seleniumEasy.page";
+
+
+const defaultTimeout = browser.params.defaultTimeout;
+
+export = function exampleSteps() {
+
+    const seleniumEasyPage = new SeleniumEasyPage;
+
+
+//Default cucumber timeout
+    this.setDefaultTimeout(600 * 1000);
+
+    //Loading browser hacks
+    let browserHacks = new BrowserHacks;
+
+    //Hooks
+    this.Before(async () => {
+        //ACTIONS BEFORE EXECUTING EACH TEST, I.E. SOME PRE-REQS FOR TEST OR SETUP
+    });
+
+    this.After(async () => {
+        //ACTIONS AFTER EXECUTING EACH TEST, I.E. CLEANUP
+        await browserHacks.ClearBrowserData();
+    });
+
+  //Checkbox  
+
+    this.Given(/^I am on Checkbox Demo Page$/, async () => {
+            await seleniumEasyPage.goTocheckboxSeleniumEasy();
+           });
+             this.Then(/^I click to "Click on this check box"$/, async () => {
+      await seleniumEasyPage.checkboxPick();
+    });
+this. Then (/^the checkbox is selected$/, async () => {
+    await seleniumEasyPage.checkboxChoice();
+
+});
+
+
+//Radiobuttons 
+
+ this.Given(/^I am on Radio Button Demo Page$/, async () => {
+      await seleniumEasyPage.goToradiobuttonSeleniumEasy();
+ });
+
+    this.Then(/^I click 'Female' RadioButton$/, async () => {
+       await seleniumEasyPage.radioPick1();
+    });
+    this.Then(/^I click '15 to 50' RadioButton$/, async () => {
+        await seleniumEasyPage.radioPick2();
+     });
+
+  this.Then(/^I click to 'Get values' button$/, async () => {
+       await seleniumEasyPage.buttonConfirm();
+
+    });
+    this.Then(/^The Radiobuttons are selected$/, async () => {
+        await seleniumEasyPage.radioSelect();
+ 
+     });
+
+//DDL
+
+
+this.Given(/^I am on DDL Demo Page$/, async () => {
+    await seleniumEasyPage.goToddlSeleniumEasy();
+});
+
+  this.Then(/^I click to "Select a day" DDL$/, async () => {
+     await seleniumEasyPage.ddlExpand();
+  });
+  this.Then(/^I click "Sunday" in expanded DDL$/, async () => {
+      await seleniumEasyPage.ddlPick();
+   });
+
+  this.Then(/^the Sunday is selected in a DDL$/, async () => {
+      await seleniumEasyPage.ddlPickconfirm();
+   });
+
+//Input
+this.Given(/^I am on Input Demo Page$/, async () => {
+    await seleniumEasyPage.goToinputSeleniumEasy();
+});
+
+  this.Then(/^I type something into an input field$/, async () => {
+     await seleniumEasyPage.typeIntoInput();
+  });
+  this.Then(/^click to "Show Message button"$/, async () => {
+      await seleniumEasyPage.buttonInputClick();
+   });
+
+  this.Then(/^there is a message is displayed$/, async () => {
+      await seleniumEasyPage.buttonInputShow();
+   });
+
+   //Submit Form
+this.Given(/^I an on Submit Form Demo Page$/, async () => {
+    await seleniumEasyPage.goTosubmitFormSeleniumEasy();
+});
+
+  this.Then(/^I type my name into the "Name" field$/, async () => {
+     await seleniumEasyPage.nameFieldTyping();
+  });
+  this.Then(/^then I click to "Submit" button$/, async () => {
+      await seleniumEasyPage.submitButtonPress();
+   });
+
+  this.Then(/^I see the loading icon animation$/, async () => {
+      await seleniumEasyPage.loadingIconSpin();
+   });
+
+   
+   //List Box Easy Demo
+this.Given(/^I am on List Box Demo Page$/, async () => {
+    await seleniumEasyPage.goTolistboxeSeleniumEasy();
+});
+
+  this.Then(/^I press to "Select All" button$/, async () => {
+     await seleniumEasyPage.buttonSelectAllElement();
+  });
+
+
+  this.Then(/^all list boxes are selected$/, async () => {
+      await seleniumEasyPage.selectedBoxesAnimation();
+   });
+
+   //File Download
+this.Given(/^I am on File Download Demo Page$/, async () => {
+    await seleniumEasyPage.goTofiledownloadSeleniumEasy();
+});
+
+  this.Then(/^I input "Example" into an input$/, async () => {
+     await seleniumEasyPage.exampleInputForDownload();
+  });
+  this.Then(/^Click to Generate button$/, async () => {
+      await seleniumEasyPage.generateButtonPress();
+   });
+
+   this.Then(/^the download button is appeared$/, async () => {
+    await seleniumEasyPage.downloadButtonNew();
+ });
+ //Numeric Calculator
+ this.Given(/^I am on input Demo Page$/, async () => {
+    await seleniumEasyPage.goToNumericInputSeleniumEasy();
+});
+
+  this.Then(/^I input "3" into an input1$/, async () => {
+     await seleniumEasyPage.firstNumericInput();
+  });
+  this.Then(/^I input "6" into an input2$/, async () => {
+      await seleniumEasyPage.secondNumericInput();
+   });
+
+   this.Then(/^I click to "Get Total" button$/, async () => {
+    await seleniumEasyPage.getTotalButtonPress();
+ });
+
+   this.Then(/^the total sum is displayed$/, async () => {
+    await seleniumEasyPage.totalSumDisplay();
+ });
+
+ //Multiple checkboxes
+ this.Given(/^I am on checkbox Demo Page$/, async () => {
+   await seleniumEasyPage.goToMultipleCheckboxPage();
+});
+
+ this.Then(/^I click to "Select All" button$/, async () => {
+    await seleniumEasyPage.selectallCheckButton();
+ });
+
+  this.Then(/^all checkboxes are selected$/, async () => {
+   await seleniumEasyPage.multipleCheckboxSelect();
+});
+
+//Keyword Filter Check
+this.Given(/^I am on Filter Check Demo Page$/, async () => {
+   await seleniumEasyPage.goToFilterList();
+});
+
+ this.Then(/^I type into an input "Tree"$/, async () => {
+    await seleniumEasyPage.filterTypeCheck();
+ });
+
+  this.Then(/^there is a matching result is shown$/, async () => {
+   await seleniumEasyPage.matchingFilterResults();
+});
+
+//Loading Bar
+this.Given(/^I am on Loading Bar Demo Page$/, async () => {
+   await seleniumEasyPage.goToloadingbarSeleniumEasy();
+});
+
+ this.Then(/^I click to "Start Download" button$/, async () => {
+    await seleniumEasyPage.startDownloadClick();
+ });
+
+  this.Then(/^the progress bar is displayed$/, async () => {
+   await seleniumEasyPage.progressBarDisplay();
+});
+
+//Modal Window
+this.Given(/^I am on Modal Demo Page$/, async () => {
+   await seleniumEasyPage.goTomodalwindowSeleniumEasy();
+});
+
+ this.Then(/^I click to "Launch Modal" button$/, async () => {
+    await seleniumEasyPage.launchModalClick();
+ });
+
+  this.Then(/^the modal is opened$/, async () => {
+   await seleniumEasyPage.modalWindowDisplay();
+});
+
+this.Then(/^I click to save the changes$/, async () => {
+   await seleniumEasyPage.saveChangesClick();
+});
+
+//Alert Message
+this.Given(/^I am on Sample Messages Page$/, async () => {
+   await seleniumEasyPage.goToalertmessageSeleniumEasy();
+});
+
+ this.Then(/^I click to Alert Message$/, async () => {
+    await seleniumEasyPage.dangerMessageClick();
+ });
+
+  this.Then(/^the Message is visible$/, async () => {
+   await seleniumEasyPage.dangerMessageDisplay();
+});
+
+this.Then(/^I click to close the Alert Message$/, async () => {
+   await seleniumEasyPage.closeButtonClick();
+});
+
+//Search & Select
+this.Given(/^I am on Search & Select Demo Page$/, async () => {
+   await seleniumEasyPage.goTosearchselectSeleniumEasy();
+});
+
+ this.Then(/^I type "In" into the input$/, async () => {
+    await seleniumEasyPage.ddlInputTyping();
+ });
+
+  this.Then(/^I pick India in the matched result$/, async () => {
+   await seleniumEasyPage.ddlValuePick();
+});
+
+this.Then(/^the choice is displayed into the DDL$/, async () => {
+   await seleniumEasyPage.ddlchoicedisplay();
+});
+
+//Date Picker
+this.Given(/^I am on Date Picker Demo Page$/, async () => {
+   await seleniumEasyPage.goTodatepickerSeleniumEasy();
+});
+
+ this.Then(/^I click to "Select Date"$/, async () => {
+    await seleniumEasyPage.selectDatePick();
+ });
+
+  this.Then(/^I click to "Today" button$/, async () => {
+   await seleniumEasyPage.todayButtonClick();
+});
+
+this.Then(/^chosen date is displayed$/, async () => {
+   await seleniumEasyPage.todayDateDisplay();
+});
+
+//IFrame
+//this.Given(/^I am on IFrame Sample Page$/, async () => {
+  // await herokuappPage.goToiframeherokuapp();
+//});
+
+ //this.Then(/^I switch to IFrame Element"$/, async () => {
+   // await herokuappPage.iFrameSwitch();
+// });
+
+ // this.Then(/^I see text placeholder "Your content goes here"$/, async () => {
+ //  await herokuappPage.placeholderVisibilityCheck();
+//});
+
+//Login Validation
+//this.Given(/^I Am on Login Validation Page$/, async () => {
+ //  await seleniumEasyPage.goTologinvalidationherokuapp();
+//});
+
+ //this.Then(/^I type valid Name"$/, async () => {
+   // await seleniumEasyPage.validNameInput();
+ //});
+
+ // this.Then(/^I type invalid surname$/, async () => {
+   //await seleniumEasyPage.invalidPasswordInput();
+//});
+
+//this.Then(/^the Error message appears$/, async () => {
+  // await seleniumEasyPage.checkValidationMessageDisplay();
+//});
+
+}
